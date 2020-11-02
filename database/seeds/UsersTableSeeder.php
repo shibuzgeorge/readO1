8039053e4c074fd79d216ff6d2dc40ac594982b0
Shibu George <shibugeorge4@aol.com>
2020-11-02 17:39:50 +0000

    Added database seeder for roles and users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        DB::table('role_user')->truncate();
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $moduleTutorRole = Role::where('name', 'module_tutor')->first();
+        $userRole = Role::where('name', 'user')->first();
+
+        $admin = User::create([
+            'name'=> 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $moduleTutor = User::create([
+            'name'=> 'Module Tutor User',
+            'email' => 'ModuleTutor@ModuleTutor.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $user = User::create([
+            'name'=> 'Generic User',
+            'email' => 'user@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $admin->roles()->attach($adminRole);
+        $moduleTutor->roles()->attach($moduleTutorRole);
+        $user->roles()->attach($userRole);
+    }
+}
