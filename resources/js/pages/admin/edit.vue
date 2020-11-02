911ee31de5982218f5cb2d64fb9c88cb21391619
Shibu George <shibugeorge4@aol.com>
2020-11-02 17:40:35 +0000

    Added admin/edit.vue and admin/users.vue

---
+++ resources/js/pages/admin/edit.vue
@@ -0,0 +1,41 @@
+<template>
+    <card :title="$t('Edit - '+user.name+'  as admin')">
+        Choose which user you'd like to give admin access<p></p>
+
+        <div class="form-check" v-for="role in roles">
+            <input type="checkbox" name="roles[]" :value="role.id"/>
+           <label>{{role.name}}</label>
+        </div>
+    <button type="submit" class="btn btn-primary">
+        Submit
+    </button>
+    </card>
+
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+
+        name: "edit",
+        data: function (){
+            return {
+                user: [],
+                roles: []
+            }
+        },
+        created() {
+            axios.get(`/api/admin/users/${this.$route.params.id}/edit`)
+                .then(response => {
+                    this.user = response.data.user;
+                        this.roles = response.data.roles;
+
+                })
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/pages/admin/users.vue
@@ -0,0 +1,21 @@
+<template>
+    <card :title="$t('Admin, User Management')">
+
+        <div class="wrapper">
+        <fetch-users/>
+        </div>
+    </card>
+</template>
+
+<script>
+    import FetchUsers from "../../components/FetchUsers";
+    export default {
+        middleware: 'auth',
+        name: "users",
+        components: {FetchUsers}
+    }
+</script>
+
+<style scoped>
+
+</style>
