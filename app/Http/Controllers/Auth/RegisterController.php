d13a98ed4da77ff7bcfddf8927b9265a17111db3
Shibu George <shibugeorge4@aol.com>
2020-12-07 00:22:51 +0000

    Edited name for role user to User

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -39,7 +39,7 @@ class RegisterController extends Controller
             return response()->json(['status' => trans('verification.sent')]);
         }
 
-        $user->roles()->attach(Role::where('name', 'user')->first());
+        $user->roles()->attach(Role::where('name', 'User')->first());
         return response()->json($user);
     }
 
