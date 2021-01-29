cc73f02b23f0ee9bc0a1371652672e3c754c68fe
Shibu George <shibugeorge4@aol.com>
2021-01-29 20:54:08 +0000

    Added db seed to set up the project properly

--- tests/Feature/SettingsTest.php
+++ tests/Feature/SettingsTest.php
@@ -14,7 +14,7 @@ class SettingsTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-
+        $this->artisan('db:seed');
         $this->user = factory(User::class)->create();
     }
 
