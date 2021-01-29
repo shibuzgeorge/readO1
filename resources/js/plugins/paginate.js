a1b4de8e7d0389ed04472c6dc99da994e25012f7
Shibu George <shibugeorge4@aol.com>
2021-01-29 03:01:49 +0000

    Added the paginate plugin and paginated the admin user management page

---
+++ resources/js/plugins/paginate.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import laravelvuepaginate from "laravel-vue-pagination"
+
+Vue.use(laravelvuepaginate);
+Vue.component('paginate', laravelvuepaginate)
+export default laravelvuepaginate
+
