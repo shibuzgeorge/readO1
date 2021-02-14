684ed71888f3b3b4c20894326d35c7ce5cadc208
Shibu George <shibugeorge4@aol.com>
2021-02-14 02:45:39 +0000

    Updated seeders and factory to add a custom quiz, question and options

--- database/seeds/TextsTableSeeder.php
+++ database/seeds/TextsTableSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Option;
+use App\Question;
+use App\Quiz;
 use App\Text;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +16,33 @@ class TextsTableSeeder extends Seeder
     public function run()
     {
 
-        Text::create([
+        $text1 = Text::create([
             'title' => 'Chapter 1',
             'description' => 'Chapter 1',
             'textbook_id' => \App\Textbook::where('title', 'Introduction to software project management')->first()->id,
             'file' => base64_encode(file_get_contents(public_path('example.pdf')))]);
+
+        $quiz_object1 = Quiz::create(['text_id' => $text1->id]);
+        $create_question1 = Question::create(['question' => 'What is software project management', 'quiz_id' => $quiz_object1->id]);
+        $create_question2 = Question::create(['question' => 'Why is software project management important', 'quiz_id' => $quiz_object1->id]);
+
+        Option::create(['option' => 'Collaboration among team members on an on-going basis',
+            'question_id' => $create_question1->id, 'points' => 0]);
+        Option::create(['option' => 'Software project management is an art and science of planning and leading software projects',
+            'question_id' => $create_question1->id, 'points' => 1]);
+        Option::create(['option' => 'Where software is created and tested',
+            'question_id' => $create_question1->id, 'points' => 0]);
+        Option::create(['option' => 'Where software is tested',
+            'question_id' => $create_question1->id, 'points' => 0]);
+
+        Option::create(['option' => 'It is needed to help create perfect software',
+            'question_id' => $create_question2->id, 'points' => 0]);
+        Option::create(['option' => 'It is needed to find the best problem solving solution',
+            'question_id' => $create_question2->id, 'points' => 0]);
+        Option::create(['option' => 'It is needed to manage team effectively',
+            'question_id' => $create_question2->id, 'points' => 1]);
+        Option::create(['option' => 'It is needed to boost team moral',
+            'question_id' => $create_question2->id, 'points' => 0]);
 
         Text::create([
             'title' => 'Chapter 2',
