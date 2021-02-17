2fe881f80b089de2d66d664e42ee023a377da58f
Shibu George <shibugeorge4@aol.com>
2021-02-17 02:53:36 +0000

    Added module factory unique for name

--- database/factories/ModuleFactory.php
+++ database/factories/ModuleFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Module::class, function (Faker $faker) {
     return [
-        'name' => $faker->sentence(2),
+        'name' => $faker->unique()->sentence(2),
         'module_code' => $faker->regexify('[A-Z]{2}').$faker->randomNumber(4),
         'year_group_id' => factory(App\YearGroup::class)->create()->id
     ];
