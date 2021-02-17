7c4b975667d87de9949c1e0bc3b1eb26ad70b50c
Shibu George <shibugeorge4@aol.com>
2021-02-17 19:58:30 +0000

    Added text for uploading textbook to extensive reading category

--- tests/Feature/TextbookTest.php
+++ tests/Feature/TextbookTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\ExtensiveReadingCategory;
 use App\Textbook;
 use App\Module;
 use App\User;
@@ -36,7 +37,7 @@ class TextbookTest extends TestCase
     }
 
     /**
-     * A test to create a textbook authorised by logging in as an Admin and Module Tutor
+     * A test to create a textbook to a module authorised by logging in as an Admin and Module Tutor
      *
      * @test
      * @return void
@@ -67,7 +68,7 @@ class TextbookTest extends TestCase
 
         $this->assertDatabaseHas('module_textbook', [
             'module_id' => $randomModule->id,
-            'textbook_id' => Textbook::where('title', 'Testing create a new textbook as admin')->first()->id,
+            'textbook_id' => Textbook::where('title', $AdminTextbook['title'])->first()->id,
         ]);
 
         //Module Tutor textbook upload
@@ -92,17 +93,17 @@ class TextbookTest extends TestCase
 
         $this->assertDatabaseHas('module_textbook', [
             'module_id' => $this->moduleTutorUser->modules()->first()->id,
-            'textbook_id' => Textbook::where('title', 'Testing create a new textbook as module tutor')->first()->id,
+            'textbook_id' => Textbook::where('title', $ModuleTutorTextbook['title'])->first()->id,
         ]);
     }
 
     /**
-     * A test to create a textbook unauthorised by logging in as a Student
+     * A test to create a textbook unauthorised to a module by logging in as a Student
      *
      * @test
      * @return void
      */
-    public function test_create_a_textbook_for_one_module_unauthorised()
+    public function test_create_a_textbook_for_one_module_unauthorised_student()
     {
         $randomModule = Module::inRandomOrder()->first();
 
@@ -123,6 +124,71 @@ class TextbookTest extends TestCase
             'title' => $textbook['title'],
             'description' => $textbook['description'],
             'file' => base64_encode(file_get_contents(public_path('example.pdf')))
+        ]);
+
+    }
+
+    /**
+     * A test to create a textbook authorised to a extensive reading category
+     * by logging in as a Admin or Module Tutor.
+     *
+     * @test
+     * @return void
+     */
+    public function test_create_a_textbook_for_one_module_authorised_extensive_reading_category()
+    {
+        $randomExtensiveReadingCategory1 = ExtensiveReadingCategory::inRandomOrder()->first();
+
+        $AdminTextbook = [
+            'title' => 'Testing create a new textbook for extensive reading admin',
+            'description' => 'This is a test for create a new textbook for extensive reading admin',
+            'file' =>  new UploadedFile(public_path('example.pdf'), 'example.pdf', 'application/pdf', null,  true),
+            'section' => 'extensiveReading',
+            'selected' => $randomExtensiveReadingCategory1->toJson()
+        ];
+
+        //Admin textbook upload
+        $this->actingAs($this->adminUser)
+            ->postJson('/api/textbook', $AdminTextbook)
+            ->assertSuccessful()
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('textbooks', [
+            'title' => $AdminTextbook['title'],
+            'description' => $AdminTextbook['description'],
+            'file' => base64_encode(file_get_contents(public_path('example.pdf')))
+        ]);
+
+        $this->assertDatabaseHas('extensive_reading_category_textbook', [
+            'extensive_reading_category_id' => $randomExtensiveReadingCategory1->id,
+            'textbook_id' => Textbook::where('title', $AdminTextbook['title'])->first()->id,
+        ]);
+
+        $randomExtensiveReadingCategory2 = ExtensiveReadingCategory::inRandomOrder()->first();
+
+        //Module Tutor textbook upload
+        $ModuleTutorTextbook = [
+            'title' => 'Testing create a new textbook for extensive reading module tutor',
+            'description' => 'This is a test for create a new textbook for extensive reading module tutor',
+            'file' =>  new UploadedFile(public_path('example.pdf'), 'example.pdf', 'application/pdf', null,  true),
+            'section' => 'extensiveReading',
+            'selected' => $randomExtensiveReadingCategory2->toJson()
+        ];
+
+        $this->actingAs($this->moduleTutorUser)
+            ->postJson('/api/textbook', $ModuleTutorTextbook)
+            ->assertSuccessful()
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('textbooks', [
+            'title' => $ModuleTutorTextbook['title'],
+            'description' => $ModuleTutorTextbook['description'],
+            'file' => base64_encode(file_get_contents(public_path('example.pdf')))
+        ]);
+
+        $this->assertDatabaseHas('extensive_reading_category_textbook', [
+            'extensive_reading_category_id' => $randomExtensiveReadingCategory2->id,
+            'textbook_id' => Textbook::where('title', $ModuleTutorTextbook['title'])->first()->id,
         ]);
 
     }
